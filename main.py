7eba4386d267c1f2175bf862e5cd24561cdf7d7e
Inu Khunger <inu.khunger@gmail.com>
2026-08-17 21:55:43 +0530

    Improve file upload error handling
    Added error handling for file upload and selection of hash algorithm.

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ def upload_file():
 
     if request.method == 'POST':
 
-        file=request.files['file']
-        choose=request.form['Choose']
+        file=request.files.get('file')
+        choose=request.form.get('Choose')
         comp_hash=request.form.get('comp_hash')
 
-        result=Hash_Gen(file,choose,comp_hash)
+        if not file or file.filename == '':
+            result={
+                "error_mesg":"Please Upload a File."}
+        elif choose == 'none':
+            result={
+                "error_mesg":"Please select an Hash Algorithm"
+            }
+
+        else:
+            result=Hash_Gen(file,choose,comp_hash)
 
     return render_template('index.html',result=result)
 
